6b4b69a39a102fbbe849c1f7b89e7fd8a6ad8776
ViccyAce <adeviccy@gmail.com>
2026-03-19 14:58:00 +0100

    fkyfgyg

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 import { AuthProvider } from "./providers/AuthProvider";
 import { router } from "./router";
-import Preloader from "./components/Preloader";
-import InstallPrompt from "./components/InstallPrompt";
 
-// TEMP: keep service worker disabled while debugging redirect/auth loops
+// TEMP: keep service worker disabled while debugging
 // if ("serviceWorker" in navigator) {
 //   window.addEventListener("load", () => {
 //     navigator.serviceWorker.register("/sw.js").catch(() => {});
@@ -15,18 +12,10 @@ import InstallPrompt from "./components/InstallPrompt";
 // }
 
 function Root() {
-  const [preloaderDone, setPreloaderDone] = useState(false);
-
   return (
-    <>
-      <Preloader onDone={() => setPreloaderDone(true)} />
-      <div style={{ visibility: preloaderDone ? "visible" : "hidden" }}>
-        <AuthProvider>
-          <RouterProvider router={router} />
-          <InstallPrompt />
-        </AuthProvider>
-      </div>
-    </>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   );
 }
 
